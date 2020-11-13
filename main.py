6af31c10aace642278d09811d530faec433f3a7d
Victor Guerra Veloso <victorgvbh@gmail.com>
2020-11-13 00:41:20 -0300

    n_modifying_commits from a file grained metric to method one + update contributors

--- main.py
+++ main.py
@@ -19,12 +19,20 @@ def main():
     TestIndex.cache_test_files(projects)
     for name, project in projects.items():
         for test_file in TestIndex.iter_test_files(name):
-            commits = modifying_commits(project.module_name + test_file, name)
-            contributors = [c.author for c in commits]
+            commits = file_modifying_commits(project.module_name + test_file, name)
             last_commit = commits[-1]
             test_file_name = test_file.rsplit('/', maxsplit=1)[-1]
             for method in list_methods(last_commit, test_file_name):
-                Method.from_pydriller_method(test_file, method, len(commits), contributors, project)
+                method_modifying_commits = set()
+                for c in commits:
+                    for m in c.modifications:
+                        if method.filename == m.filename:
+                            if method in m.methods:
+                                method_modifying_commits.add(c)
+                        else:
+                            break
+                contributors = {c.author for c in method_modifying_commits}
+                Method.from_pydriller_method(test_file, method, len(method_modifying_commits), contributors, project)
     with open('projects.json', 'w') as f:
         f.write(jsonpickle.encode(projects))
 
@@ -37,7 +45,7 @@ def list_methods(commit, filename, prefix=""):
                     yield method
 
 
-def modifying_commits(relative_path, name):
+def file_modifying_commits(relative_path, name):
     repo = pydriller.RepositoryMining(f"./projects/{name}", filepath=relative_path)
     return list(repo.traverse_commits())
 
